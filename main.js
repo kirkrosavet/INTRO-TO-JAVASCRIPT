d464fc23ddffba62ae5499919821b78c2e7e7e45
kirkrosavet <majestyreignsak47@gmail.com>
2025-05-12 14:15:54 -0700

    solved questions

---
+++ main.js
@@ -0,0 +1,13 @@
+// Use alert(...); to display 'Good morining!' in a popup.
+
+// Display your name in a popup 
+
+
+
+// Use math, calucluate 10 + 5 in the console 
+console.log(10+5)
+
+
+// Calculate 20 -5 in the console
+console.log(20-5)
+
