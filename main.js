c3ecaa9c168d0745a11f9b750c2ba7fe06857a8a
kirkrosavet <majestyreignsak47@gmail.com>
2025-05-12 14:19:47 -0700

    alert

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 // Use alert(...); to display 'Good morining!' in a popup.
 
+alert("Good morning")
+
 // Display your name in a popup 
 
-
+alert("kirk rosavet")
 
 // Use math, calucluate 10 + 5 in the console 
 console.log(10+5)
