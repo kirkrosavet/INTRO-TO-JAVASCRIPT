19c2b364ba5ee3805a780738a6af289c78d3fd0b
kirkrosavet <majestyreignsak47@gmail.com>
2025-05-12 14:42:10 -0700

    names

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
 // Use alert(...); to display 'Good morining!' in a popup.
 
-alert("Good morning")
+// alert("Good morning")
 
 // Display your name in a popup 
 
-alert("kirk rosavet")
+// alert("kirk rosavet")
 
 // Use math, calucluate 10 + 5 in the console 
-console.log(10+5)
+console.log(10+5);
 
 
 // Calculate 20 -5 in the console
-console.log(20-5)
+console.log(20-5);
 
+//Create a variable to store your name and log it to the console.
+let myName = "kirk rosavet"; 
+console.log(myName);
+
+
+
+//Concatenate your first and last name and log the result.
+let firstName = "kirk";
+let lastName = "rosavet";
+let fullName = firstName + " " + lastName;
+console.log(myName);
+
+
+//Find and log the length of a string stored in a variable.
+
+//Convert a string to a number and a number to a string, logging the results.
