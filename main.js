fc8f356e50a714b3432c0d45873d3e42b341f0af
kirkrosavet <majestyreignsak47@gmail.com>
2025-05-15 11:59:24 -0700

    strings

--- main.js
+++ main.js
@@ -21,11 +21,54 @@ console.log(myName);
 
 //Concatenate your first and last name and log the result.
 let firstName = "kirk";
-let lastName = "rosavet";
-let fullName = firstName + " " + lastName;
-console.log(myName);
+let lastName = "Rosavet";
+// let fullName = `My name is 'firstName' + " " + 'lastName`;
+console.log(`My name is ${firstName} ${lastName}`);
 
 
 //Find and log the length of a string stored in a variable.
 
 //Convert a string to a number and a number to a string, logging the results.
+
+let age = (23)
+console.log(age)
+
+ pi = 3.14;
+pi = 10;
+
+console.log(pi);
+
+let a = (5)
+let b = (10)
+console.log(a+b)
+
+ string = "reapper"
+console.log(string)
+
+const single ='blake'
+const double ="bellamy"
+const backtick =`playboy`
+
+console.log(single)
+console.log(double)
+console.log(backtick)
+console.log(single + double)
+
+const name = "kirk"
+const greeting = `hello, ${name}`;
+console.log(greeting)
+
+const n = "KIRK"
+const one ="hello" 
+const two =" how are you doing"
+const joined = `${one}${n}${two}`;
+console.log(joined)
+
+const button = document.querySelector("button");
+
+function greet() {
+    const name = prompt("what is your name? ");
+    const greeting = document.querySelector("#greeting");
+greeting.textContent =`hello ${name}, nice to see you`;
+}
+button.addEventListener("click", greet);
